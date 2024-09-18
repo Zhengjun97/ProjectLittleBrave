89bd8b49b4e2a9498920deaab351fd331127f255
Zhengjun97 <zhengjunzheng1127@gmail.com>
2024-09-18 19:17:09 -0400

    loading combat background and loading assets

---
+++ src/scenes/preload-scene.js
@@ -0,0 +1,45 @@
+
+import { BATTLE_ASSET_KEYS, BATTLE_BACKGROUND_ASSET_KEYS, HEALTH_BAR_ASSET_KEYS, MONSTER_ASSET_KEYS } from "../assets/asset-keys.js";
+import Phaser from "../lib/phaser.js";
+import { SCENE_KEYS } from "./scene-keys.js";
+
+export class PreloadScene extends Phaser.Scene{
+    constructor() {
+        super({
+            key: SCENE_KEYS.PRELOAD_SCENE, //unique key for pharse scene
+        });
+        console.log(SCENE_KEYS.PRELOAD_SCENE);  
+    }
+
+    //phaser life cycle events
+
+
+
+    preload() {
+        
+        const mosterTamerAssetPath = 'assets/images/monster-tamer';
+        const kenneysAssetPath = 'assets/images/kenneys-assets';
+        
+        //battle backgrounds
+        this.load.image(BATTLE_BACKGROUND_ASSET_KEYS.FOREST, `${mosterTamerAssetPath}/battle-backgrounds/forest-background.png`);
+        //battle assets
+        this.load.image(BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND, `${kenneysAssetPath}/ui-space-expansion/custom-ui.png`);
+        //health bar assets
+        this.load.image(HEALTH_BAR_ASSET_KEYS.RIGHT_CAP, `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_right.png`);
+        this.load.image(HEALTH_BAR_ASSET_KEYS.MIDDLE, `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_mid.png`);
+        this.load.image(HEALTH_BAR_ASSET_KEYS.LEFT_CAP, `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_left.png`);
+
+        //monster assets
+        this.load.image(MONSTER_ASSET_KEYS.CARNODUSK, `${mosterTamerAssetPath}/monsters/carnodusk.png`);
+        this.load.image(MONSTER_ASSET_KEYS.IGUANIGNITE, `${mosterTamerAssetPath}/monsters/iguanignite.png`);
+        
+        //console.log(`${mosterTamerAssetPath}/battle-backgrounds/forest-background.png`);
+    }
+
+    create() {
+        console.log('create');
+        console.log(this.textures.get('background'));
+        this.add.image(0,0,BATTLE_BACKGROUND_ASSET_KEYS.FOREST).setOrigin(0);
+    }
+
+}
