c8466065e286f241c1ede5e67bf88cd86fc7c370
Kevin Cen Feng <kevincen27@gmail.com>
2024-10-01 22:13:36 -0400

    update the call to the player character with the new created player class

--- src/scenes/battle-scene.js
+++ src/scenes/battle-scene.js
@@ -3,6 +3,7 @@ import { DIRECTION } from "../common/direction.js";
 import Phaser from "../lib/phaser.js";
 import { Background } from "./battle/background.js";
 import { EnemyBattleMonster } from "./battle/monsters/enemy-battle-monster.js";
+import { PlayerBattleMonster } from "./battle/monsters/player-battle-monster.js";
 import { BattleMenu } from "./battle/ui/menu/battle-menu.js";
 import { HealthBar } from "./battle/ui/menu/health-bar.js";
 import { SCENE_KEYS } from "./scene-keys.js";
@@ -15,6 +16,9 @@ export class BattleScene extends Phaser.Scene{
 
     /** @type {EnemyBattleMonster}*/
     #activeEnemyMonster;
+    
+    /** @type {PlayerBattleMonster}*/
+    #activePlayerMonster;
 
     constructor() {
         super({
@@ -41,33 +45,24 @@ export class BattleScene extends Phaser.Scene{
                 currentHp: 25,
                 maxHp: 25,
                 attackIds: [],
-                baseAttack: 5
-            }
-        })
-        //this.add.image(768,144,MONSTER_ASSET_KEYS.CARNODUSK,(0)).setFlipX(true).setScale(0.5);
-        this.add.image(256,316,MONSTER_ASSET_KEYS.IGUANIGNITE,(0)).setScale(2);
+                baseAttack: 5,
+                currentLevel: 5
+            },
+        });
 
-        //render out the player HP bar
-        const playerHealthBar = new HealthBar(this, 34, 34);
-        const playerMsName = this.add.text(30,20,'Main Character', {color: '#7E3D3F', fontSize: '32px'});
-        this.add.container(556,318,[this.add.image(0,0,BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND).setOrigin(0)
-            , playerMsName, 
-            playerHealthBar.container,
-            this.add.text(playerMsName.width + 35,23,'L5', {color: '#ED474B', fontSize: '28px'}),
-            this.add.text(30,55,'HP', {color: '#FF6505', fontSize: '24px', fontStyle: 'italic'}),
-            this.add.text(443,80,'25/25', {color: '#7E3D3F', fontSize: '16px',}).setOrigin(1,0),
-            ]);
-
-        //render out the enemy HP bar
-        //const enemyHealthBar = new HealthBar(this, 34, 34);
-        const enemyHealthBar = this.#activeEnemyMonster._healthBar;
-        const enemyMsName = this.add.text(30,20,'Dark Knight', {color: '#7E3D3F', fontSize: '32px'});
-        this.add.container(0,0,[this.add.image(0,0,BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND).setOrigin(0)
-            , enemyMsName, 
-            enemyHealthBar.container,
-            this.add.text(enemyMsName.width + 35,23,'L5', {color: '#ED474B', fontSize: '28px'}),
-            this.add.text(30,55,'HP', {color: '#FF6505', fontSize: '24px', fontStyle: 'italic'}),
-            ]);
+        this.#activePlayerMonster = new PlayerBattleMonster({
+            scene: this,
+            monsterDetails: {
+                name: MONSTER_ASSET_KEYS.IGUANIGNITE,
+                assetKey: MONSTER_ASSET_KEYS.IGUANIGNITE,
+                assetFrame: 0,
+                currentHp: 25,
+                maxHp: 25,
+                attackIds: [],
+                baseAttack: 5,
+                currentLevel: 5
+            },
+        });
             
         //render out the main info and sub info panes
         this.#battleMenu = new BattleMenu(this);
@@ -75,14 +70,8 @@ export class BattleScene extends Phaser.Scene{
 
         this.#cursorKeys = this.input.keyboard.createCursorKeys();
 
-        playerHealthBar.setMeterPercentageAnimated(0.5,{
-            duration: 3000,
-            callback: () =>{
-                console.log('Animation Completed');
-            }
-        });
         //this is to check if takeDamage function is working or not, may DELETE later
-        // this.#activeEnemyMonster.takeDamage(12);
+         this.#activeEnemyMonster.takeDamage(17, ()=>{this.#activePlayerMonster.takeDamage(13);});
         // console.log(this.#activeEnemyMonster.isFainted);
     }
 
