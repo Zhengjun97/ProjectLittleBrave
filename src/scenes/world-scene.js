5a680521bc41030c83611de359aa7ff58df0e7d7
Zhengjun97 <zhengjunzheng1127@gmail.com>
2024-10-25 16:09:19 -0400

    Added logic to move npc

--- src/scenes/world-scene.js
+++ src/scenes/world-scene.js
@@ -285,6 +285,21 @@ export class WorldScene extends Phaser.Scene {
                 return;
             }
 
+            //get the path objects for this npc
+            const pathObjects = layer.objects.filter((obj) => {
+                return obj.type === CUSTOM_TILED_TYPES.NPC_PATH;
+            });
+            const npcPath = {
+                0: { x: npcObject.x, y: npcObject.y - TILE_SIZE }
+            };
+            pathObjects.forEach((obj) => {
+                if (obj.x === undefined || obj.y === undefined) {
+                    return;
+                }
+                npcPath[parseInt(obj.name, 10)] = { x: obj.x, y: obj.y - TILE_SIZE };
+            });
+            console.log(npcPath);
+
             /** @type {string} */
             const npcFrame = 
             /** @type {TiledObjectProperty[]}*/(npcObject.properties).find((property) => property.name === TILED_NPC_PROPERTY.FRAME)?.value || '0';
@@ -294,6 +309,9 @@ export class WorldScene extends Phaser.Scene {
             /** @type {TiledObjectProperty[]}*/(npcObject.properties).find((property) => property.name === TILED_NPC_PROPERTY.MESSAGES)?.value || '0';
             const npcMessages = npcMessagesString.split('::');
 
+            const npcMovement = 
+            /** @type {TiledObjectProperty[]}*/(npcObject.properties).find((property) => property.name === TILED_NPC_PROPERTY.MOVEMENT_PATTERN)?.value || 'IDLE';
+
             //in tiled, the x vaule is how far the object starts from the left, and the y is the bottom of tiled
             const npc = new NPC({
                 scene: this,
@@ -301,6 +319,8 @@ export class WorldScene extends Phaser.Scene {
                 direction: DIRECTION.DOWN,
                 frame: parseInt(npcFrame, 10),
                 messages: npcMessages,
+                npcPath,
+                movementPattern: npcMovement,
             })
             this.#npcs.push(npc);
         });
--- src/world/character/npc.js
+++ src/world/character/npc.js
@@ -4,10 +4,26 @@ import { exhaustiveGuard } from "../../utils/guard.js";
 import { Character } from "./character.js";
 
 /**
+ * @typedef {keyof typeof NPC_MOVEMENT_PATTERN} NpcMovementPattern
+ */
+/** @enum {NpcMovementPattern} */
+export const NPC_MOVEMENT_PATTERN = Object.freeze({
+    IDLE: 'IDLE',
+    CLOCKWISE: 'CLOCKWISE'
+});
+
+/**
+ * @typedef NPCPath
+ * @type {Object.<number, import("../../types/typedef.js").Coordinate>}
+ */
+
+/**
  * @typedef NPCConfigProps
  * @type {object}
  * @property {number} frame
  * @property {string[]} messages
+ * @property {NPCPath} npcPath
+ * @property {NpcMovementPattern} movementPattern
  */
 
 /**
@@ -19,26 +35,36 @@ export class NPC extends Character {
     #messages;
     /** @type {boolean} */
     #talkingToPlayer;
+    /** @type {NPCPath} */
+    #npcPath;
+    /** @type {number} */
+    #currentPathIndex;
+    /** @type {NpcMovementPattern} */
+    #movementPattern;
+
     /**
-     * @param {NPCConfig} config
-     */
+    * @param {NPCConfig} config
+    */
     constructor(config) {
-      super({
-        ...config,
-        assetKey: CHARACTER_ASSET_KEYS.NPC,
-        origin: { x: 0, y: 0 },
-        idleFrameConfig: {
-          DOWN: config.frame,
-          UP: config.frame + 1,
-          NONE: config.frame,
-          LEFT: config.frame + 2,
-          RIGHT: config.frame + 2,
-        },
-      });
-      
-      this.#messages = config.messages;
-      this.#talkingToPlayer = false;
-      this._phaserGameObject.setScale(4);
+        super({
+            ...config,
+            assetKey: CHARACTER_ASSET_KEYS.NPC,
+            origin: { x: 0, y: 0 },
+            idleFrameConfig: {
+                DOWN: config.frame,
+                UP: config.frame + 1,
+                NONE: config.frame,
+                LEFT: config.frame + 2,
+                RIGHT: config.frame + 2,
+            },
+        });
+
+        this.#messages = config.messages;
+        this.#talkingToPlayer = false;
+        this.#npcPath = config.npcPath;
+        this.#currentPathIndex = 0;
+        this.#movementPattern = config.movementPattern;
+        this._phaserGameObject.setScale(4);
     }
 
     /** @type {string[]} */
@@ -62,8 +88,8 @@ export class NPC extends Character {
      * @param {import("../../common/direction.js").Direction} playerDirection
      * @returns {void} 
      */
-    facePlayer(playerDirection){
-        switch(playerDirection){
+    facePlayer(playerDirection) {
+        switch (playerDirection) {
             case DIRECTION.DOWN:
                 this._phaserGameObject.setFrame(this._idleFrameConfig.UP).setFlipX(false);
                 break;
@@ -81,5 +107,44 @@ export class NPC extends Character {
             default:
                 exhaustiveGuard(playerDirection);
         }
+    }
+
+    /**
+     * @param {DOMHighResTimeStamp} time 
+     * @returns {void}
+     */
+    update(time) {
+        if (this._isMoving) {
+            return;
+        }
+        if (this.#talkingToPlayer) {
+            return;
+        }
+        super.update(time);
+
+        if(this.#movementPattern === NPC_MOVEMENT_PATTERN.IDLE) {
+            return;
+        }
+
+        /** @type {import("../../common/direction.js").Direction} */
+        let characterDirection = DIRECTION.NONE;
+        let nextPosition = this.#npcPath[this.#currentPathIndex +1];
+        if (nextPosition === undefined) {
+            nextPosition = this.#npcPath[0];
+        } else {
+            this.#currentPathIndex = this.#currentPathIndex + 1;
+        }
+
+        if (nextPosition.x > this._phaserGameObject.x) {
+            characterDirection = DIRECTION.RIGHT;
+        } else if (nextPosition.x < this._phaserGameObject.x) {
+            characterDirection = DIRECTION.LEFT;
+        } else if (nextPosition.y < this._phaserGameObject.y) {
+            characterDirection = DIRECTION.UP;
+        } else if (nextPosition.y > this._phaserGameObject.y) {
+            characterDirection = DIRECTION.DOWN;
+        } 
+        this.moveCharacter(characterDirection);
+
     }
 }
